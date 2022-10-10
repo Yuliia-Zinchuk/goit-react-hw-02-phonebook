135f3ad646bce5427a34f5a02262c208c5f4d12e
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-10-10 07:37:05 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,10 +8,10 @@ import { Filter } from './Filter/Filter';
 export class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
@@ -52,20 +52,21 @@ export class App extends Component {
 
   render() {
     const { contacts, filter } = this.state;
+    const { onAddContact, handleChange, onDeleteContact } = this;
 
     return (
       <>
         <Section title="Phonebook">
-          <ContactsForm onSubmit={this.onAddContact} />
+          <ContactsForm onSubmit={onAddContact} />
         </Section>
 
         <Section title="Contacts">
-          {<Filter handleChange={this.handleChange} />}
+          {<Filter handleChange={handleChange} />}
           {contacts && (
             <ContactsList
               contacts={contacts}
               filter={filter}
-              onDeleteContact={this.onDeleteContact}
+              onDeleteContact={onDeleteContact}
             />
           )}
         </Section>
